d1211a54c77781372f7ea497310274418195354c
mayanksharma18 <mmayank.sharma18@gmail.com>
2022-07-27 18:49:02 +0530

    remove commented code

--- Problems/symmetricDifference.js
+++ Problems/symmetricDifference.js
@@ -2,11 +2,16 @@
    either of the two sets but not in both. 
 For example, for sets A = {1, 2, 3} and B = {2, 3, 4}, A △ B = {1, 4}. 
 Symmetric difference is a binary operation, which means it operates on only two elements.
+
+-Symmetric Difference basically contains all elements of two arrays except common elements. 
 */
 
 function sym(args) {
     let  argsIn = [...arguments];
     let res = [];
+    if(argsIn.length < 1) {
+      throw new Error('Please enter one array');
+    }
     for(let i=0;  i< argsIn.length;){
       if(i===0){
         res=symDifference(argsIn[i],argsIn[i+1]);
@@ -18,42 +23,6 @@ function sym(args) {
     }
     return res;
 }
-  
-// function symDifference(input1, input2) {
-//     let symRes = [];
-//     let maxInput = [...input1];
-//     let minInput = [...input2];
-  
-//     if (input1.length < input2.length) {
-//       maxInput = [...input2];
-//       minInput = [...input1];
-//     }
-//     for (let i = 0; i < maxInput.length; i++) {
-//       let count = 0;
-//       for (let j = 0; j < minInput.length; j++) {
-//         if (minInput[j] === maxInput[i]) {
-//           count++;
-//         }
-//       }
-//       if (count === 0 && symRes.indexOf(maxInput[i]) === -1) {
-//         symRes.push(maxInput[i]);
-//       }
-//     }
-//     for(let i=0; i< minInput.length; i++){
-//       let count = 0;
-//       for(let j=0; j < maxInput.length; j++){
-//         if(maxInput[j] ===  minInput[i]){
-//           count++;
-//         }
-//       }
-//       if(count === 0 && symRes.indexOf(minInput[i]) === -1){
-//         symRes.push(minInput[i]);
-//       }
-//     }
-//     return symRes.sort() // todo
-  
-//   }
-  
   function symDifference(input1, input2) {
     let symRes = [];
     let maxInput = [...input1];
@@ -72,18 +41,6 @@ function sym(args) {
   function getSymDifference(input1,input2){
       let resArr = [];
 
-    //   for (let i = 0; i < input1.length; i++) {
-    //     let count = 0;
-    //     for (let j = 0; j < input2.length; j++) {
-    //       if (input2[j] === input1[i]) {
-    //         count++;
-    //       }
-    //     }
-    //     if (count === 0 && resArr.indexOf(input1[i]) === -1) {
-    //         resArr.push(input1[i]);
-    //     }
-    //   }
-
     // use of for loop
     for (const element1 of input1) {
         let count = 0;
@@ -99,5 +56,4 @@ function sym(args) {
       return resArr;
 
   }
-
   console.log(sym([1, 2, 3], [5, 2, 1, 4])); // [3,4,5]
